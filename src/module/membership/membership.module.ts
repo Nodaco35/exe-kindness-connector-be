da0e04422a8999acd22e9896f41f02a047154c29
thuanlv0911 <thuanlv0911@gmail.com>
2026-06-17 23:37:04 +0700

    feat: codeqrcode membership

--- src/module/membership/membership.module.ts
+++ src/module/membership/membership.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { MembershipService } from './membership.service';
 import { MembershipController } from './membership.controller';
+import { Membership, MembershipSchema } from './entities/membership.entity';
+import { User, UserSchema } from '../user/entities/user.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Membership.name, schema: MembershipSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [MembershipController],
   providers: [MembershipService],
+  exports: [MembershipService],
 })
 export class MembershipModule {}
